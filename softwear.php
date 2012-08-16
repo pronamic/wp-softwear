d9badb95409736f819dd96f605be34e3d0babed9
Remco Tolsma <info@remcotolsma.nl>
2012-08-16 16:45:52 +0200

    WordPress Coding Standards optimization.

--- softwear.php
+++ softwear.php
@@ -12,21 +12,21 @@ Author URI: http://pronamic.eu/
 License: GPL
 */
 
-if ( function_exists( 'spl_autoload_register' ) ):
+if ( function_exists( 'spl_autoload_register' ) ) {
 
-function pronamic_softwear_autoload( $name ) {
-	$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
-	$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
-
-	$file = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
-
-	if ( is_file( $file ) ) {
-		require_once $file;
+	function pronamic_softwear_autoload( $name ) {
+		$name = str_replace( '\\', DIRECTORY_SEPARATOR, $name );
+		$name = str_replace( '_', DIRECTORY_SEPARATOR, $name );
+	
+		$file = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $name . '.php';
+	
+		if ( is_file( $file ) ) {
+			require_once $file;
+		}
 	}
+	
+	spl_autoload_register( 'pronamic_softwear_autoload' );
+	
+	Pronamic_Softwear_Plugin::bootstrap( __FILE__ );
+
 }
-
-spl_autoload_register( 'pronamic_softwear_autoload' );
-
-Pronamic_Softwear_Plugin::bootstrap( __FILE__ );
-
-endif;
